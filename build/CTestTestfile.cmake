fa79935cfac8cf8eebe6be92ee7cd7d6e1652155
KhachDavid <khachatryandavid1@gmail.com>
2025-03-18 20:38:21 -0500

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/egeyilmaz/JoltCUDA/Build
-# Build directory: /home/egeyilmaz/JoltCUDA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[UnitTests]=] "UnitTests")
-set_tests_properties([=[UnitTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/egeyilmaz/JoltCUDA/Build/CMakeLists.txt;380;add_test;/home/egeyilmaz/JoltCUDA/Build/CMakeLists.txt;0;")
